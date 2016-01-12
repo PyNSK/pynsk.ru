075b80d504bc1a08ec6bdf4b968b6970cdfa6aef
Alexander Sapronov <a@sapronov.me>
2016-01-12 14:00:14 +0600

    Прикрутил Mezzanine

--- project/settings.py
+++ project/settings.py
@@ -18,7 +18,7 @@ LANGUAGE_CODE = 'ru-ru'
 SITE_ID = 1
 USE_I18N = True
 USE_L10N = True
-USE_TZ = False
+USE_TZ = True
 MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
 
 MEDIA_URL = '/media/'
@@ -49,35 +49,57 @@ TEMPLATES = [
                 'django.core.context_processors.request',
                 'apps.meetup.context.menu',
                 'apps.meetup.context.all_events_processor',
-
+                "django.template.context_processors.tz",
                 'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.i18n',
                 'django.template.context_processors.request',
                 'django.contrib.messages.context_processors.messages',
                 'zinnia.context_processors.version',  # Optional
+                "mezzanine.conf.context_processors.settings",
+                "mezzanine.pages.context_processors.page",
             ],
             'loaders': [
                 'django.template.loaders.filesystem.Loader',
                 'django.template.loaders.app_directories.Loader',
-            ]
+            ],
         }
     }
 ]
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
-    'django.middleware.common.CommonMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+
+    "mezzanine.core.middleware.UpdateCacheMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
+    # Uncomment if using internationalisation or localisation
+    # 'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #    'debug_toolbar.middleware.DebugToolbarMiddleware',
+
+    "mezzanine.core.request.CurrentRequestMiddleware",
+    "mezzanine.core.middleware.RedirectFallbackMiddleware",
+    "mezzanine.core.middleware.TemplateForDeviceMiddleware",
+    "mezzanine.core.middleware.TemplateForHostMiddleware",
+    "mezzanine.core.middleware.AdminLoginInterfaceSelectorMiddleware",
+    "mezzanine.core.middleware.SitePermissionMiddleware",
+    # Uncomment the following if using any of the SSL settings:
+    # "mezzanine.core.middleware.SSLRedirectMiddleware",
+    "mezzanine.pages.middleware.PageMiddleware",
+    "mezzanine.core.middleware.FetchFromCacheMiddleware",
+
 )
 
 AUTHENTICATION_BACKENDS = (
     #    'admin_sso.auth.DjangoSSOAuthBackend',
     'django.contrib.auth.backends.ModelBackend',
+    "mezzanine.core.auth_backends.MezzanineBackend",
 )
 
 ROOT_URLCONF = 'project.urls'
@@ -87,7 +109,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 TEMPLATE_DIRS = ('',)
 
 INSTALLED_APPS = (
-    'suit',
+    # 'suit',
 
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -96,58 +118,42 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    "django.contrib.redirects",
+    "django.contrib.sitemaps",
 
-    'djangobower',
-    'storages',
-    'pytils',
-    'googlecharts',
-    'sslserver',
-    'django_comments',
-    'mptt',
-    'tagging',
+    # 'djangobower',
+    # 'storages',
+    # 'pytils',
+    # 'googlecharts',
+    # 'sslserver',
+    # 'django_comments',
+    # 'mptt',
+    # 'tagging',
     'zinnia',
-    'django_pygments',
+    # 'django_pygments',
 
-    'apps.meetup',
-    'apps.subscribers',
+    "mezzanine.boot",
+    "mezzanine.conf",
+    "mezzanine.core",
+    "mezzanine.generic",
+    "mezzanine.pages",
+    "mezzanine.blog",
+    "mezzanine.forms",
+    "mezzanine.galleries",
+    "mezzanine.twitter",
+    # "mezzanine.accounts",
+
+    # 'apps.meetup',
+    # 'apps.subscribers',
     'apps.frontend',
     'apps.tasks',
 
-    # 'apps.articles',
 )
 
 # if DEBUG:
 #    INSTALLED_APPS += ('debug_toolbar',)
 
-ALLOWED_HOSTS = ['.project.ru', 'moscowdjango-staging.herokuapp.com', 'localhost', '.moscowpython.ru']
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
-        'console': {
-            'level': 'ERROR',
-            'class': 'logging.StreamHandler'
-        },
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins', 'console'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
+ALLOWED_HOSTS = ['*']
 
 INTERNAL_IPS = ('127.0.0.1',)
 
@@ -190,7 +196,24 @@ ZINNIA_MARKDOWN_EXTENSIONS = [
     'markdown.extensions.wikilinks'
 ]
 
+USE_MODELTRANSLATION = False
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+FILE_UPLOAD_PERMISSIONS = 0o644
+# Full filesystem path to the project.
+PROJECT_APP_PATH = os.path.dirname(os.path.abspath(__file__))
+PROJECT_APP = os.path.basename(PROJECT_APP_PATH)
+PROJECT_ROOT = BASE_DIR = os.path.dirname(PROJECT_APP_PATH)
+CACHE_MIDDLEWARE_KEY_PREFIX = PROJECT_APP
+PACKAGE_NAME_FILEBROWSER = "filebrowser_safe"
+PACKAGE_NAME_GRAPPELLI = "grappelli_safe"
 
+OPTIONAL_APPS = (
+    "debug_toolbar",
+    "django_extensions",
+    "compressor",
+    PACKAGE_NAME_FILEBROWSER,
+    PACKAGE_NAME_GRAPPELLI,
+)
 
 BOWER_INSTALLED_APPS = (
     'jquery#2',
@@ -198,6 +221,43 @@ BOWER_INSTALLED_APPS = (
     'bootstrap-material-design',
     'underscore',
 )
+
+TEMPLATE_CONTEXT_PROCESSORS = TEMPLATES[0]['OPTIONS']['context_processors']
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console': {
+            'level': 'ERROR',
+            'class': 'logging.StreamHandler'
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins', 'console'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+try:
+    from mezzanine.utils.conf import set_dynamic_settings
+except ImportError:
+    pass
+else:
+    set_dynamic_settings(globals())
 
 try:
     from .local_settings import *
--- project/urls.py
+++ project/urls.py
@@ -1,17 +1,18 @@
-from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
+from django.conf.urls import patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic.base import TemplateView
+from django.views.i18n import set_language
+from mezzanine.conf import settings
+from mezzanine.core.views import direct_to_template
 
 from .settings import STATIC_ROOT, ROOT_PATH
-
 
 admin.autodiscover()
 
 handler500 = TemplateView.as_view(template_name="500.html")
-
 
 blog_urls = [
     url(r'^', include('zinnia.urls.capabilities')),
@@ -30,22 +31,37 @@ blog_urls = [
     url(r'^blog/', include('zinnia.urls.quick_entry'))
 ]
 
-urlpatterns = patterns('',
-    url(r'^(favicon.ico)$', 'django.views.static.serve', {'document_root': STATIC_ROOT}),
-    url(r'^(robots.txt)$', 'django.views.static.serve', {'document_root': STATIC_ROOT}),
-    url(r'^humans.txt$', 'django.views.static.serve', {'document_root': ROOT_PATH, 'path': 'AUTHORS.txt'}),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'gnfdgdsf', include('apps.meetup.urls', namespace='meetup')),
-    url(r'hgfhfdghfghf', include('apps.frontend.urls', namespace='frontend')),
-    url(r'', include('apps.tasks.urls', namespace='tasks')),
-    url(r'^(?P<filename>[\w-]+\.(?:html|txt))$', 'apps.meetup.views.confirm_ownership', name='ownership'),
-    # url(r'^weblog/', include('zinnia.urls', namespace='zinnia')),
-    # url(r'^', include(blog_urls, namespace='zinnia')),
-    url(r'^comments/', include('django_comments.urls')),
+urlpatterns = patterns(
+        '',
+        url(r'^(favicon.ico)$', 'django.views.static.serve', {'document_root': STATIC_ROOT}),
+        url(r'^(robots.txt)$', 'django.views.static.serve', {'document_root': STATIC_ROOT}),
+        url(r'^humans.txt$', 'django.views.static.serve', {'document_root': ROOT_PATH, 'path': 'AUTHORS.txt'}),
+        url(r'^admin/', include(admin.site.urls)),
+
+        url(r'gnfdgdsf', include('apps.meetup.urls', namespace='meetup')),
+        url(r'hgfhfdghfghf', include('apps.frontend.urls', namespace='frontend')),
+        url(r'', include('apps.tasks.urls', namespace='tasks')),
+
+
+        # url(r'^weblog/', include('zinnia.urls', namespace='zinnia')),
+        # url(r'^', include(blog_urls, namespace='zinnia')),
+        # url(r'^comments/', include('django_comments.urls')),
 )
 
+if settings.USE_MODELTRANSLATION:
+    urlpatterns += [
+        url('^i18n/$', set_language, name='set_language'),
+    ]
 
+urlpatterns += [
+    url("^blog$", direct_to_template, {"template": "index.html"}, name="home"),
+    url("^blog", include("mezzanine.urls")),
+]
 
+# Adds ``STATIC_URL`` to the context of error pages, so that error
+# pages can use JS, CSS and images.
+handler404 = "mezzanine.core.views.page_not_found"
+handler500 = "mezzanine.core.views.server_error"
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
