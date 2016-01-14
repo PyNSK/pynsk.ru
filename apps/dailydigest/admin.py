fb827b00a571976c7ed02486003a8ea28b861fb0
Alexander Sapronov <a@sapronov.me>
2016-01-14 15:27:03 +0600

    Добавил поля отображения в админке для модели вводного текста дайджеста

--- apps/dailydigest/admin.py
+++ apps/dailydigest/admin.py
@@ -4,5 +4,24 @@ from django.contrib import admin
 
 from apps.dailydigest.models import DailyIssue, InitialText
 
+
+class InitialTextAdmin(admin.ModelAdmin):
+    list_display = [
+        'content',
+        'is_active',
+        'weight'
+    ]
+
+    list_filter = [
+        'is_active',
+        'weight'
+    ]
+
+    list_editable = [
+        'is_active',
+        'weight',
+    ]
+
+
 admin.site.register(DailyIssue)
-admin.site.register(InitialText)
+admin.site.register(InitialText, InitialTextAdmin)
