ef65c888dfa0b14f77f10549a83048288d527b40
Alexander Sapronov <a@sapronov.me>
2016-02-08 00:33:25 +0600

    ADD: добавил возможность из админки сменить автора поста

--- apps/blog_theme/admin.py
+++ apps/blog_theme/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from mezzanine.blog.admin import BlogPostAdmin
+from mezzanine.blog.models import BlogPost
+
+admin.site.unregister(BlogPost)
+
+custom_fieldset = BlogPostAdmin.fieldsets
+custom_fieldset[0][1]["fields"].append('user')
+
+
+class CustomBlogPostAdmin(BlogPostAdmin):
+    # list_editable = BlogPostAdmin.list_editable + ('user',)
+    fieldsets = custom_fieldset
+
+
+admin.site.register(BlogPost, CustomBlogPostAdmin)
