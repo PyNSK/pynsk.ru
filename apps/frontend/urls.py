19d29d4ff1f4e37ae287ecf32f4945c3b00cf8e3
Alexander Sapronov <a@sapronov.me>
2016-02-22 01:55:16 +0600

    ADD: добавил кнопку "поддержать" в навигацию и страницу с донатом

--- apps/frontend/urls.py
+++ apps/frontend/urls.py
@@ -3,12 +3,12 @@
 
 from django.conf.urls import patterns, url
 
-from apps.frontend.views import ThanksPage, AboutPage
+from apps.frontend.views import ThanksPage, AboutPage, DonatePage
 
 urlpatterns = patterns(
-        '',
-        url(r'^thanks$', ThanksPage.as_view(), name='thanks'),
-        url(r'^about$', AboutPage.as_view(), name='about'),
-
+    '',
+    url(r'^thanks$', ThanksPage.as_view(), name='thanks'),
+    url(r'^about$', AboutPage.as_view(), name='about'),
+    url(r'^donate$', DonatePage.as_view(), name='donate'),
 
 )
--- apps/frontend/views.py
+++ apps/frontend/views.py
@@ -10,5 +10,9 @@ class AboutPage(TemplateView):
     template_name = 'pages/about.html'
 
 
+class DonatePage(TemplateView):
+    template_name = 'pages/donate.html'
+
+
 class IndexPage(TemplateView):
     template_name = 'home.html'
